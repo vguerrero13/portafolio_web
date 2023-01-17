aadebb0c23c8b352e5f58f3dd6ab04c123977222
Valentin Guerrero <122841919+vguerrero13@users.noreply.github.com>
2023-01-17 11:53:26 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+    "use strict";
+
+ 
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true,
+        });
+    }
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+   
+
+  
+})(jQuery);
